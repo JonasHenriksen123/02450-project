8692c91f9a5d2b63e77dd03cb9b2521906d1f062
JonasHenriksen123 <56156281+JonasHenriksen123@users.noreply.github.com>
2021-10-04 18:30:17 +0200

    div

--- __main__.py
+++ __main__.py
@@ -3,14 +3,17 @@ import source.pcanalysis as pca
 
 if __name__ == '__main__':
 
+    # fetch data through data object
     data = Da.Data()
 
-    #pca.plot_attribute_against(data, 4, 7, '')
+    # region generate plots
+    pca.plot_attribute_against(data, 4, 7, 'forestfires FFMC v ISI')
 
-    #pca.plot_visualized_data(data, '')
+    pca.plot_visualized_data(data, 'forestfires principal component explained variance')
 
-    pca.plot_visualized_coefficients(data, 3, '', legend=True)
+    pca.plot_visualized_coefficients(data, 3, 'forestfires pca coefficients visualized', legend=True)
 
-    #pca.plot_boxplots(data, 'forestfires boxplots')
+    pca.plot_boxplots(data, 'forestfires boxplots')
 
-    #pca.plot_boxplot(data, 6, 'forestfires boxplot DC')
+    pca.plot_boxplot(data, 6, 'forestfires boxplot DC')
+    # endregion
